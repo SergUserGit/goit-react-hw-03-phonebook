967622d0e5ad21bf64d12c93926bde8d95fe10c7
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-09 11:51:15 +0200

    start develop
    start develop

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,64 @@
+import css from './ContactForm.module.css';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+  state = { name: '', number: '' };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form className={css.formContact} onSubmit={this.handleSubmit}>
+        <label className={css.formLabel}>
+          Name
+          <input
+            className={css.formInput}
+            value={this.state.name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className={css.formLabel}>
+          Number
+          <input
+            className={css.formInput}
+            value={this.state.number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button className={css.formButton} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
